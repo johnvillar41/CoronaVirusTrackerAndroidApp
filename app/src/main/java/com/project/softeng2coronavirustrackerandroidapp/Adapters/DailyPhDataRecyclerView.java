e076027f7be8013ca6c61d4320ba37c546498dbc
Villar <johnvillar41@yahoo.com>
2021-03-14 18:55:17 +0800

    Added gifs on DailyPhDataRecyclerView

--- app/src/main/java/com/project/softeng2coronavirustrackerandroidapp/Adapters/DailyPhDataRecyclerView.java
+++ app/src/main/java/com/project/softeng2coronavirustrackerandroidapp/Adapters/DailyPhDataRecyclerView.java
@@ -1,6 +1,5 @@
 package com.project.softeng2coronavirustrackerandroidapp.Adapters;
 
-import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -16,6 +15,7 @@ import com.project.softeng2coronavirustrackerandroidapp.R;
 
 import java.text.DecimalFormat;
 import java.text.NumberFormat;
+import java.util.ArrayList;
 import java.util.List;
 
 public class DailyPhDataRecyclerView extends RecyclerView.Adapter<DailyPhDataRecyclerView.MyViewHolder> {
@@ -33,17 +33,23 @@ public class DailyPhDataRecyclerView extends RecyclerView.Adapter<DailyPhDataRec
                 .inflate(R.layout.recyclerview_dailyphdata, parent, false);
         return new MyViewHolder(view);
     }
-
+    
     @Override
     public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
         DailyPhStatusModel dailyPhStatusModel = getItem(position);
         NumberFormat formatter = new DecimalFormat("###,###,###");
-
         try {
             if (getItem(position).getInfected() < getItem(position - 1).getInfected()) {
-                holder.lottieAnimationView.setVisibility(View.VISIBLE);
                 int increase = getItem(position - 1).getInfected() - getItem(position).getInfected();
-                holder.txtInfectedIncrease.setText(String.valueOf(increase));
+                int averageIncrease = computeAverageOfIncrease();
+                if (increase > averageIncrease) {
+                    holder.increaseGif.setVisibility(View.VISIBLE);
+                    holder.decreaseGif.setVisibility(View.INVISIBLE);
+                } else {
+                    holder.increaseGif.setVisibility(View.INVISIBLE);
+                    holder.decreaseGif.setVisibility(View.VISIBLE);
+                }
+                holder.txtInfectedIncrease.setText(String.valueOf(formatter.format(increase)));
             }
         } catch (ArrayIndexOutOfBoundsException ignored) {
 
@@ -59,6 +65,19 @@ public class DailyPhDataRecyclerView extends RecyclerView.Adapter<DailyPhDataRec
         });
     }
 
+    private int computeAverageOfIncrease() {
+        int total = 0;
+        int average = 0;
+        for (int i = 0; i < list.size(); i++) {
+            try {
+                total += (list.get(i - 1).getInfected() - list.get(i).getInfected());
+            } catch (ArrayIndexOutOfBoundsException ignored) {
+            }
+        }
+        average = total / list.size();
+        return average;
+    }
+
     private DailyPhStatusModel getItem(int position) {
         return list.get(position);
     }
@@ -71,16 +90,17 @@ public class DailyPhDataRecyclerView extends RecyclerView.Adapter<DailyPhDataRec
     public static class MyViewHolder extends RecyclerView.ViewHolder {
 
         private TextView txtTotalInfected, txtDate, txtInfectedIncrease;
-        private LottieAnimationView lottieAnimationView;
+        private LottieAnimationView increaseGif, decreaseGif;
         private Button btnSeeMore;
 
         public MyViewHolder(@NonNull View itemView) {
             super(itemView);
             txtTotalInfected = itemView.findViewById(R.id.txtTotalInfected);
             txtDate = itemView.findViewById(R.id.txtDate);
-            lottieAnimationView = itemView.findViewById(R.id.increase);
+            increaseGif = itemView.findViewById(R.id.increase);
             btnSeeMore = itemView.findViewById(R.id.btnSeeMore);
             txtInfectedIncrease = itemView.findViewById(R.id.txtInfectedIncrease);
+            decreaseGif = itemView.findViewById(R.id.decrease);
         }
     }
 }
